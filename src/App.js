07fdba035e19eaf41a7758d6df122c79caea525b
BShanmukharao <barrishanmukha28@gmail.com>
2025-01-27 20:12:34 +0530

    solutions

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserManagement from "./components/UserManagement";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center p-4">
+      <UserManagement />
+    </div>
+  );
+}
+
+export default App;
